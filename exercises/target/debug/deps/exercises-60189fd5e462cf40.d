5a928de00c4bc3c208fb527d184832723ad83b5a
Pablo@panelaLaptop <adacanet@protomail.com>
2024-07-01 12:35:22 -0600

    First commit

---
+++ exercises/target/debug/deps/exercises-60189fd5e462cf40.d
@@ -0,0 +1,5 @@
+/home/pablo/git/rust_in_motion/exercises/target/debug/deps/exercises-60189fd5e462cf40.rmeta: src/main.rs
+
+/home/pablo/git/rust_in_motion/exercises/target/debug/deps/exercises-60189fd5e462cf40.d: src/main.rs
+
+src/main.rs:
