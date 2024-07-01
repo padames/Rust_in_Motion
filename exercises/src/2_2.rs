f744b651575cfc9b9925b499ad51967e024d7ab7
Pablo@panelaLaptop <adacanet@protomail.com>
2024-07-01 12:46:22 -0600

    skeleton ready

--- exercises/src/2_2.rs
+++ exercises/src/2_2.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("book");
+    // Add code here that calls the pluralize function
+    println!(
+        "I have one {}, you have two {}",
+        s,
+        // you add something here
+    );
+}
+
+//Add approprieate parameters, return values, and implementation to this function
+fn pluralize()
+{
+
 }
