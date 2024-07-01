dde9ec8ed08e89c15129418c2cffdf1a35424385
Pablo@panelaLaptop <adacanet@protomail.com>
2024-07-01 13:41:38 -0600

    more solutions

--- exercises/src/main_2_2.rs
+++ exercises/src/main_2_2.rs
@@ -7,6 +7,13 @@ fn main() {
         s,
         ss // you add something here
     );
+
+    println!(
+        "Now done even more idiomatically...
+        I have one {}, you have two {}",
+        s,
+        pluralize_idiomatic_plus(&s)
+    )
 }
 
 //Add approprieate parameters, return values, and implementation to this function
@@ -15,4 +22,9 @@ fn pluralize(s: &String) ->String
     let mut ss = String::from(s);
     ss.push_str("s");
     ss
+}
+
+fn pluralize_idiomatic_plus(s: &str) ->String
+{
+    s.to_owned() + "s"
 }
