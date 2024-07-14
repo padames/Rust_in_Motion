fb292b1e4eccacfa7ee38573e98ee85cc8f26b62
Pablo@panelaLaptop <adacanet@protomail.com>
2024-07-13 20:48:19 -0600

    example from video on burrowing patterns

---
+++ burrows/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut list = vec![1, 2, 3];
+
+    let first_element = list.first();
+
+    println!("The first element is {:?}", first_element);
+ 
+    *list.first_mut().expect("list was empty") += 1;
+}
