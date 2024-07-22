995000f9aabd2f8937f7ce5f5e68711fd53f034a
padames <pablo.adames@gmail.com>
2024-07-21 22:54:08 -0600

    borrowing well

--- borrows/src/main.rs
+++ borrows/src/main.rs
@@ -6,4 +6,6 @@ fn main() {
     println!("The first element is {:?}", first_element);
  
     *list.first_mut().expect("list was empty") += 1;
+
+    println!("The first element after adding one is {:?}", list.first() );
 }
